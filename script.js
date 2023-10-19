4254d1ea9512180fc38ba1ffd7b0de8b26618af1
CRNaro <144285637+CRNaro@users.noreply.github.com>
2023-10-19 15:37:42 -0400

    Notes: Added pseudocode for my own personal reference to help me remember what and why I added the code

--- script.js
+++ script.js
@@ -30,20 +30,20 @@ function generatePassword(length, numbers, uppercase, lowercase, symbols) {
 
 
 
-    // 
+    // Character iteration string
     let characters = '';
     for (let i = 0; i < multiSelect.length; i++) {
         characters += multiSelect[i];
     }
 
-    //
+    // Password generation string - using the random math and lenght chosen 
     let password = '';
     for (let i = 0; i < length; i++) {
         password += characters.charAt(Math.floor(Math.random() * characters.length));
     }
     return password;
 }
-
+// Popup and questions 
 function writePassword() {
     const uppercase = confirm('Password SHOULD include an uppercase letter.  Click OK to continue');
     const lowercase = confirm('Password SHOULD include a lowercase letter.  Click OK to continue');
@@ -51,9 +51,11 @@ function writePassword() {
     const symbols = confirm('Password SHOULD include a symbol letter.  Click OK to continue');
     const keyLength = prompt('Password must be between 8 and 128 characters.  Click OK to continue');
 
+    // Runs generatePassword with what the user wanted
     const password = generatePassword(keyLength, uppercase, lowercase, numbers, symbols);
 
+    // Puts password element value in so it can be generated
     passwordText.value = password;
 }
-
+//  addEventListener is to connect the 'click' of the button and run the generating
 generateBtn.addEventListener('click', writePassword);
